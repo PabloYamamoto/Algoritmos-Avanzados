10c9b1c4fabed27c18f5fff33d7a23933cc864e3
SkohnE <s.kohne99@gmail.com>
2021-09-10 15:16:07 -0500

    algoritmo terminado

--- Homework - Boyer-Moore/main.cpp
+++ Homework - Boyer-Moore/main.cpp
@@ -2,59 +2,77 @@
 #include <string>
 #include <vector>
 #include <map>
+
 using namespace std;
-
-// HERE IS A SIMPLE EXAMPLE
-// EXAMPLE
-// 0123456
-
-// R[6][P] = 4
-
-// formula para movernos es m+= index - R[index][T[k]] -> 6 - 4 = 2
-
-// bad char rule
-
-// R[index][T[k]] -> entero
-
-// Collection of lists
-// Non repeating chars of the pattern
-// E - 6, 0
-// X - 1
-// A - 2
-// M - 3
-// P - 4
-// L - 5
 
 int badCharacterRule(map<char, vector<int> > &R, int i, char T)
 {
-    if (R.find(T) != R.end())
+    if (R.find(T) != R.end()) // check if the letter exists.
     {
         for (int k = R[T].size() - 1; k >= 0; k--)
         {
             if (R[T][k] < i)
             {
-                return i - R[T][k];
+                return i - R[T][k]; 
             }
         }
     }
-    return i + 1;
+    return i + 1; // return the size of the move that we have to do.
 }
 
 vector<int> booyerMore(string &txt, string &pattern)
 {
+    int size = pattern.length();
     vector<int> ans;
-    map<char, vector<int> > Bad; // map with letters with index in pattern.
 
-    for (size_t i = 0; i < pattern.length(); i++)
+    // Bad Charachter preprocessing
+    map<char, vector<int> > Bad; // map with letters with index in pattern.
+    for (int i = 0; i < size; i++)
     {
+        //fill the map and vectors.
         Bad[pattern[i]].push_back(i);
     }
 
-    int i = pattern.length() - 1; // pattern
-    int j = 0;                    // text
-    int s, m;                     // Where we store the max of bad character rule or good suffix rule
+    // Good suffix preprocessing
+    vector<int> table(size, -1);
+    table[size - 1] = size - 2;
 
-    while (j < txt.length() - pattern.length() + 1)
+    int i = size - 1;
+    int j = size - 2;
+    
+    while (j > -1)
+    {
+        if (pattern[i] == pattern[j])
+        {
+            if (table[i] == -1)
+            {
+                table[i] = j;
+            }
+            i--;
+            j--;
+        }
+        else
+        {
+            i = size - 1;
+            j--;
+        }
+    }
+
+    int aux;
+    for (int i = size -1; i >= 0; i--)
+    {
+        if (table[i] == -1)
+        {
+            table[i] = aux;
+        }
+        aux = table[i];
+    }
+    
+    i = size - 1; // pattern
+    j = 0;                    // text
+    int s, m;                 // Where we store the max of bad character rule or good suffix rule
+
+    while (j < txt.length() - size + 1)
     {
         // Compare P to T[k ... P.length()] from right to left
         while (txt[j + i] == pattern[i] && i > 0)
@@ -64,14 +82,15 @@ vector<int> booyerMore(string &txt, string &pattern)
         if (i == 0 && txt[j] == pattern[i])
         {
             ans.push_back(j);
-            m = pattern.length() - 1;
+            m = size - 1;
         }
         else
         {
-            m = badCharacterRule(Bad, i, txt[j + i]);
+            // good suffix  -  Bad character
+            m = max(size - table[i] - 1, badCharacterRule(Bad, i, txt[j + i]));
         }
 
-        i = pattern.length() - 1;
+        i = size - 1;
         j += m;
         // Max(good, bad)
     }
